22f0ccdd528443ac84af247584f5a034cc9577bc
Govinda Pandeya <gowinnpandey@gmail.com>
2017-04-24 16:38:23 -0500

    configured succesfully

--- src/main/java/com/mum/group2/repository/QuestionRepository.java
+++ src/main/java/com/mum/group2/repository/QuestionRepository.java
@@ -13,6 +13,72 @@ import com.mum.group2.domain.Question;
  *
  */
 @Repository
-public interface QuestionRepository extends CrudRepository<Question, Long> {
+public class QuestionRepository implements CrudRepository<Question, Long> {
+
+	@Override
+	public <S extends Question> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Question> Iterable<S> save(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Question findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean exists(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterable<Question> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Question> findAll(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Question entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Iterable<? extends Question> entities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
